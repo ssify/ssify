d5c124b7138e998c937797fdb0baa03e8f2787f4
Coding dragon <79044272+JiteshChichole1@users.noreply.github.com>
2023-11-04 22:49:47 +0530

    adding files
    Signed-off-by: Coding dragon <79044272+JiteshChichole1@users.noreply.github.com>

---
+++ public/js/index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get all song links
+    const songLinks = document.querySelectorAll('.songname');
+
+    // Attach click event listener to each song link
+    songLinks.forEach(function (songLink) {
+        songLink.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            // Get the selected song data attributes
+            const ds = this.getAttribute('zx-ds');
+            const sn = this.getAttribute('zx-dsn');
+            const dsan = this.getAttribute('zx-dsan');
+            const dspu = this.getAttribute('zx-dspu');
+
+            const songData = `${ds}|${sn}|${dsan}|${dspu}`;
+
+            // Set a cookie with the song data
+            document.cookie = `songData=${encodeURIComponent(songData)}`;
+            // Redirect to songplayer.html with the selected song data as a query parameter
+            window.location.href = ``;
+        });
+    });
+});
